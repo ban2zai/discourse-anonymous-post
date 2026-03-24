1a67f27c7f99de9b17a962e04c278e7c86d8c734
ban2zai <aleskerov.02@gmail.com>
2026-03-24 11:36:23 +0700

    fix v11

--- assets/javascripts/discourse/initializers/anonymous-post.js
+++ assets/javascripts/discourse/initializers/anonymous-post.js
@@ -7,18 +7,14 @@ export default apiInitializer("0.3.0", (api) => {
 
   api.renderInOutlet("composer-fields-below", AnonymousPostCheckbox);
 
-  api.includePostAttributes("is_anonymous_post");
-
   api.addPostClassesCallback((attrs) => {
     return attrs.is_anonymous_post ? ["is-anonymous-post"] : [];
   });
 
-  api.decorateWidget("poster-name:after", (helper) => {
-    const attrs = helper.attrs;
-    if (attrs && attrs.is_anonymous_post) {
-      return helper.h("span.anon-post-indicator", [
-        helper.iconNode("ghost"),
-      ]);
+  api.addPosterIcons((cfs, attrs) => {
+    if (attrs.is_anonymous_post) {
+      return [{ icon: "ghost", className: "anon-post-indicator" }];
     }
+    return [];
   });
 });
